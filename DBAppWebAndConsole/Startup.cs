94ea8a4da5de49c3448e12ddc9cda3d494bab64e
Vafeen <666av6@gmail.com>
2024-12-01 17:42:24 +0300

    remove useless

--- DBAppWebAndConsole/Startup.cs
+++ DBAppWebAndConsole/Startup.cs
@@ -17,22 +17,9 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error"); 
-            app.UseHsts();
-        }
-
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-
         app.UseRouting();
-
-        app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
         {
